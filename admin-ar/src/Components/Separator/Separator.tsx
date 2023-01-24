5f38b283e804626354b43df916e82d59fd7c23cd
Kisus228 <nik.babushkin.201408@gmail.com>
2023-01-25 01:18:43 +0500

    Теперь в сеператор делает auto на неизвестные величины + можно передать цвет

--- admin-ar/src/Components/Separator/Separator.tsx
+++ admin-ar/src/Components/Separator/Separator.tsx
@@ -1,19 +1,23 @@
 import styles from "./Separator.module.scss";
 
 interface SeparatorProps {
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
+  color?: string;
 }
 
 export function Separator(props: SeparatorProps) {
-  const { width, height } = props;
+  const color = props.color ?? "";
+  const width = props.width ? `${props.width}px` : "auto";
+  const height = props.height ? `${props.height}px` : "auto";
 
   return (
     <div
       className={styles.separator}
       style={{
-        width: `${width}px`,
-        height: `${height}px`,
+        width: width,
+        height: height,
+        backgroundColor: color,
       }}
     ></div>
   );

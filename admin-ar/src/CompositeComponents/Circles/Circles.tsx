106d3fcfe5c750538aa23d4d4a00b5ba948b3266
Kisus228 <nik.babushkin.201408@gmail.com>
2023-01-24 19:08:32 +0500

    Первый шаг в адаптивную вёрстку

--- admin-ar/src/CompositeComponents/Circles/Circles.tsx
+++ admin-ar/src/CompositeComponents/Circles/Circles.tsx
@@ -2,9 +2,9 @@ import styles from "./Circles.module.scss"
 import {cn} from "../../classnames";
 
 export function Circles() {
-    return <>
+    return <div className={styles.circles_wrapper}>
         <div className={cn(styles.circle, styles.circle_1)}></div>
         <div className={cn(styles.circle, styles.circle_2)}></div>
         <div className={cn(styles.circle, styles.circle_3)}></div>
-    </>
+    </div>
 }
--- admin-ar/src/CompositeComponents/Footer/Footer.tsx
+++ admin-ar/src/CompositeComponents/Footer/Footer.tsx
@@ -2,41 +2,43 @@ import styles from "./Footer.module.scss";
 
 export function Footer() {
   return (
-    <div className={styles.footer_wrapper}>
-      <div className={styles.footer_blocks}>
-        <Block
-          heading={"ThreeXyNine++"}
-          elements={[
-            "Команда, которая всегда стремится к совершенству, чтобы обрадовать своих пользователей.",
-          ]}
-        />
-        <Block heading={"Портфолио"} elements={["Сад", "Огород", "Прочее"]} />
-        <Block
-          heading={"Клиентам"}
-          elements={["Помощь", "Контакты", "О нас"]}
-        />
-        <Block
-          heading={"Контакты"}
-          elements={[
-            <div className={styles.contact}>
-              <img
-                className={styles.contact_image}
-                src={"./Resources/Contacts/mail.svg"}
-              />
-              <p>AR_Garden@gmail.com</p>
-            </div>,
-            <div className={styles.contact}>
-              <img
-                className={styles.contact_image}
-                src={"./Resources/Contacts/phone.svg"}
-              />
-              <p>8-800-555-35-35</p>
-            </div>,
-          ]}
-        />
-      </div>
-      <div className={styles.footer_copyright}>
-        Copyright ©️ 2023 Build. Все права защищены | ThreeXyNine++ Team
+    <div className={styles.footer_background}>
+      <div className={styles.footer_wrapper}>
+        <div className={styles.footer_blocks}>
+          <Block
+            heading={"ThreeXyNine++"}
+            elements={[
+              "Команда, которая всегда стремится к совершенству, чтобы обрадовать своих пользователей.",
+            ]}
+          />
+          <Block heading={"Портфолио"} elements={["Сад", "Огород", "Прочее"]} />
+          <Block
+            heading={"Клиентам"}
+            elements={["Помощь", "Контакты", "О нас"]}
+          />
+          <Block
+            heading={"Контакты"}
+            elements={[
+              <div className={styles.contact}>
+                <img
+                  className={styles.contact_image}
+                  src={"./Resources/Contacts/mail.svg"}
+                />
+                <p>AR_Garden@gmail.com</p>
+              </div>,
+              <div className={styles.contact}>
+                <img
+                  className={styles.contact_image}
+                  src={"./Resources/Contacts/phone.svg"}
+                />
+                <p>8-800-555-35-35</p>
+              </div>,
+            ]}
+          />
+        </div>
+        <div className={styles.footer_copyright}>
+          Copyright ©️ 2023 Build. Все права защищены | ThreeXyNine++ Team
+        </div>
       </div>
     </div>
   );
